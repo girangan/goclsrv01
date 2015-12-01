94ba8f9b91b798cb6f2f42367b1fae68ec379ac7
Girish <girangan@cisco.com>
2015-12-01 13:47:31 -0500

    minor updates

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+    "time"
 )
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
+    
+    if !keepBusy{
+        keepBusy = true
+        fmt.Println("Keeping the process busy", time.Now())
+        time.Sleep(1 * time.Second)
+    }
 }
+
+var keepBusy bool
 
 func main() {
     http.HandleFunc("/", defaultHandler)
